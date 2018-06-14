9a91aeb5240f70e14aae422bee7db4412633d731
Susanne <suzze-t@hotmail.com>
2018-06-14 15:06:32 +0200

    ..

--- data-retriever/get_detailed_match_data.py
+++ data-retriever/get_detailed_match_data.py
@@ -1,16 +1,38 @@
 import numpy as np
 import urllib.request
+import time
+import json
+import csv
 
 
 def main():
-  # TODO: load file containing match ids that we want detailed match data for
-  match_id_file = 'match_ids_for_latest_matches.csv'
-  data = np.loadtxt(match_id_file, delimiter=',')
+  # Load file containing match ids that we want detailed match data for
+  filename = 'match_ids_for_latest_matches.csv'
+  data = np.genfromtxt(filename, dtype=np.dtype('i8'), skip_header=1)
   
-  for match_id in range(len(data)):  #Example of match_id = 3921060078
-    url = "https://api.opendota.com/api/matches/" + match_id 
-    data = urllib.request.urlopen(url).read()
+  with open("./%s" % ('detailed_match_data.csv'), 'w', newline='',  encoding="utf-8") as outf:
+    # Write header into csv file
+    url = "https://api.opendota.com/api/matches/" + str(data[1])
+    header = json.loads(urllib.request.urlopen(url).read()).keys()
+    dw = csv.DictWriter(outf, header)
+    dw.writeheader()
+    
+    for i in range(len(data)):
+      # Avoid exceeding the rate limit of 60 calls per minute
+      # See https://www.opendota.com/api-keys
+      if (i % 50 == 0 and i != 0):
+        print(str(i) + '/' + str(i) + ' iterations done...\n')
+        time.sleep(70)
+      
+      # Get detailed match data and write into csv file
+      match_id = data[i]
+      url = "https://api.opendota.com/api/matches/" + str(match_id)
+      detailed_data = json.loads(urllib.request.urlopen(url).read())
+      dw.writerow(detailed_data)
     
     
     # TODO make a new file where it's possible to take a subset
     # of the fields within the detailed match data...
+  
+    
+main()
