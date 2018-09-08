a581c3494e19825e815fd6098897e999a13520ca
Bjarke Kingo <bjarkekingo50@yahoo.dk>
2018-09-08 19:28:11 +0200

    Update hypothesis_testing.py

--- hypothesis_testing.py
+++ hypothesis_testing.py
@@ -1,6 +1,8 @@
 import math
 import numpy as np
 import scipy.stats as stats
+import matplotlib.pyplot as plt
+import sys
 
 """
 Let X and Y be random variables describing the gold or exp advantage of radiant and dire team.
@@ -20,11 +22,10 @@ def hypothesis_testing(X, Y, alpha):
   
   # Compute p-value
   p = stats.t.cdf(-abs(t_statistic), dof) * 2
-  print(p)
   
   # Reject null hypothesis if p < alpha
   alpha = 0.05
-  return (p < alpha)
+  return p#(p < alpha)
   
 
 """
@@ -49,31 +50,70 @@ def test_hypothesis(radiant_win, gpm, xpm, alpha):
       loser_xpm.append(xpm[i])      
       
   gpm_hypothesis = hypothesis_testing(winner_gpm, loser_gpm, alpha)
-  print('\ngpm calculation:', gpm_hypothesis)
+  #print('\ngpm calculation:', gpm_hypothesis)
   xpm_hypothesis = hypothesis_testing(winner_xpm, loser_xpm, alpha)
-  print('\nxpm calculation:', xpm_hypothesis)
-  return gpm_hypothesis and xpm_hypothesis
+  #print('\nxpm calculation:', xpm_hypothesis)
+  return [gpm_hypothesis, xpm_hypothesis]
   
 
 def main():
-  data = np.genfromtxt('data-retriever/datasets/9_gold_exp_advantage_matrix.csv',delimiter=',',skip_header=1)
+  data = np.genfromtxt('9_gold_exp_advantage_matrix.csv',delimiter=',',skip_header=1)
   alpha = 0.05
   
   result = []
-  for i in range(15):
+  for i in range(int(np.max(data[:,1]))):
     duration = data[:, 1]
     winning_team  = data[np.where(duration == i)[0]][:,1]
+    if(len(winning_team)<50):
+        continue
     gpm_advantage = data[np.where(duration == i)[0]][:,2]
     xpm_advantage = data[np.where(duration == i)[0]][:,3]
     
     result.append(test_hypothesis(winning_team, gpm_advantage, xpm_advantage, alpha))
   
-  print(result)
-  
-  
-  
-  
-main()
-  
-  
-  
+  gpm_hypotheses = [result[i][0] for i in range(len(result))]
+  xpm_hypotheses = [result[i][1] for i in range(len(result))]
+
+  return result, gpm_hypotheses, xpm_hypotheses 
+
+result, gpm_hypotheses, xpm_hypotheses = main()
+
+alpha=0.05
+plt.figure(figsize=(12,6))
+def minhandler(number):
+    if(number>0):
+        return number
+    else:
+        return sys.float_info.min
+
+fail_to_reject = list(np.where(np.array(gpm_hypotheses) > alpha)[0]) + list(np.where(np.array(xpm_hypotheses) > alpha)[0])
+fail_to_reject_values = [gpm_hypotheses[i] for i in np.where(np.array(gpm_hypotheses) > alpha)[0]] + [xpm_hypotheses[i] for i in np.where(np.array(xpm_hypotheses) > alpha)[0]]
+
+xpm_hypotheses = [minhandler(xpm_hypotheses[i]) for i in range(len(xpm_hypotheses))]
+plt.plot(gpm_hypotheses, label='gpm p-values', linewidth=3.5, color='#53E53C')
+plt.plot(xpm_hypotheses, label='xpm p-values', linewidth=3.0, color='orange')
+plt.plot([alpha for i in range(len(xpm_hypotheses))], label=('alpha = ' + str(alpha)), color='black')
+plt.scatter(fail_to_reject, fail_to_reject_values, label='Failed to reject hypothesis', color='red')
+plt.title('p-values from hypothesis-testing gpm and xpm values per game-time minute')
+plt.xticks(np.arange(0,len(result),2.0))
+plt.yticks(np.arange(0,1.1,0.1))
+plt.xlabel('Game minute')
+plt.ylabel('p-value')
+plt.legend()
+plt.show()
+
+log_fail_to_reject = list(np.where(np.array([math.log(gpm_hypotheses[i]) for i in range(len(gpm_hypotheses))]) > math.log(alpha))[0]) + list(np.where(np.array([math.log(xpm_hypotheses[i]) for i in range(len(xpm_hypotheses))]) > math.log(alpha))[0])
+log_fail_to_reject_values = [[math.log(gpm_hypotheses[i]) for i in range(len(gpm_hypotheses))][i] for i in np.where(np.array([math.log(gpm_hypotheses[i]) for i in range(len(gpm_hypotheses))]) > math.log(alpha))[0]] + [[math.log(xpm_hypotheses[i]) for i in range(len(xpm_hypotheses))][i] for i in np.where(np.array([math.log(xpm_hypotheses[i]) for i in range(len(xpm_hypotheses))]) > math.log(alpha))[0]]
+
+plt.figure(figsize=(12,6))
+plt.plot([math.log(gpm_hypotheses[i]) for i in range(len(gpm_hypotheses))], label='log(gpm p-values)', linewidth=3.5, color='#53E53C')
+plt.plot([math.log(xpm_hypotheses[i]) for i in range(len(xpm_hypotheses))], label='log(xpm p-values)', linewidth=3.0, color='orange')
+plt.plot([math.log(alpha) for i in range(len(xpm_hypotheses))], label=('alpha = log(' + str(alpha) + ')'), color='black')
+plt.scatter(log_fail_to_reject, log_fail_to_reject_values, label='Failed to reject hypothesis', color='red')
+plt.title('log(p-values) from hypothesis-testing gpm and xpm values per game-time minute')
+plt.xticks(np.arange(0,len(result),2.0))
+plt.ticklabel_format(style='sci', axis='y', scilimits=(0,0))
+plt.xlabel('Game minute')
+plt.ylabel('log(p-value)')
+plt.legend()
+plt.show()
