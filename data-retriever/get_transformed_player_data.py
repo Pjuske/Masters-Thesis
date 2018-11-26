1045d1e24068d76f98f5c8e685b6050651a5f8ea
Susanne <suzze-t@hotmail.com>
2018-11-26 22:00:43 +0100

    fixed some bugs for gpm/xpm calculation

--- data-retriever/get_transformed_player_data.py
+++ data-retriever/get_transformed_player_data.py
@@ -1,21 +1,42 @@
 import numpy as np
 import json
 import csv
+import ast
 
 def CustomParser(data):
     j1 = json.loads(data)
     return j1
 
 
+def get_team_scores(radiant_score, dire_score, player_slot):
+  # If player is on team Dire, return their score for the feature 'my_team_score'
+  if player_slot > 4:
+    return dire_score, radiant_score
+  else:
+    return radiant_score, dire_score
+  
+
+def get_final_gpm_xpm(gpm_data, xpm_data, player_slot):
+  # Get the gold/xpm difference when the game ended
+  gpm = gpm_data[-1]
+  xpm = xpm_data[-1]
+  
+  # If player is on team Dire, negate the gpm and xpm values
+  if player_slot > 4:
+    return gpm*(-1),xpm*(-1)
+  else:
+    return gpm, xpm
+
 
 def main():
   playerColumns = ['player0','player1','player2','player3','player4',
                    'player5','player6','player7','player8','player9']
- 
-  team_feats    = ['radiant_score','dire_score','radiant_gold_adv','radiant_xp_adv']
+  
   player_feats  = ['hero_id','kills','deaths','assists','hero_healing',
                    'last_hits','gold_per_min','xp_per_min','tower_damage',
                    'hero_damage','obs_placed','sen_placed']
+  
+  team_feats    = ['my_team_score','enemy_team_score','my_team_gold_adv','my_team_xp_adv']
 
 
   # Reads the detaled match data, ignoring those with missing values
@@ -36,22 +57,29 @@ def main():
 
     for i in range(len(json_list)):
       if (i % 3000 == 0):
-          print(i*10, 'iterations done...\n')
+          print(i, 'iterations done...\n')
       
       try:
-        row = json_list[i]
-        my_dict={}
+        my_dict = {}
+        row     = json_list[i]
         
         for player in playerColumns:  
           # Parse player data and add it to dicitonary
           player_data = json.loads(row[player].replace("\'", "\""))
+          player_slot = player_data['player_slot']
+          
           for feature in player_feats:
             my_dict[feature] = player_data[feature]
             
           # Add team data to dictionary
-          for feature in team_feats:
-            my_dict[feature] = json.loads(row[feature])  
+          my_score, enemy_score       = get_team_scores(json.loads(row['radiant_score']),json.loads(row['dire_score']),player_slot)
+          my_gold_avg, my_xpm_avg     = get_final_gpm_xpm(json.loads(row['radiant_gold_adv']),json.loads(row['radiant_xp_adv']),player_slot)
           
+          my_dict['my_team_score']    = my_score
+          my_dict['enemy_team_score'] = enemy_score
+          my_dict['my_team_gold_adv'] = my_gold_avg
+          my_dict['my_team_xp_adv']   = my_xpm_avg
+               
           # Write row
           dw.writerow(my_dict)
             
